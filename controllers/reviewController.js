4329f346f392a3f047a6f8ff08a222ec9ea51072
ahad-iftikhar <169081213+ahad-iftikhar@users.noreply.github.com>
2024-08-20 10:33:01 +0500

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -10,36 +10,6 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
-// exports.createReview = catchAsync(async (req, res, next) => {
-//   // For nested routing
-//   if (!req.body.tour) req.body.tour = req.params.tourId;
-//   if (!req.body.user) req.body.user = req.user.id;
-
-//   const newReview = await Review.create(req.body);
-
-//   res.status(201).json({
-//     status: 'success',
-//     data: {
-//       review: newReview,
-//     },
-//   });
-// });
-
-// exports.getAllReviews = catchAsync(async (req, res, next) => {
-//   let filter = {}; // For nested route, for getting reviews for a specific tour
-//   if (req.params.tourId) filter = { tour: req.params.tourId };
-
-//   const reviews = await Review.find(filter);
-
-//   res.status(200).json({
-//     status: 'success',
-//     reviews: reviews.length,
-//     data: {
-//       reviews,
-//     },
-//   });
-// });
-
 exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
